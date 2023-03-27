beb999bc37a2af38e3a1515e606547fa6b1aa3c7
Henshaw <josephhensh2000@gmail.com>
2023-03-27 10:16:33 -0700

    Update file: write function name correctly

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _is_upper - function that checks for uppercase character
+ * _isupper - function that checks for uppercase character
  *
  * @c: the character to check
  *
  * Return: 1 if c is uppercase, 0 otherwise
  */
-int _is_upper(int c)
+int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (1);
