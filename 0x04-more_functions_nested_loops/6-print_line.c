3643f9158c7e60a13027ea1e02c685fd5a109d8e
Henshaw <josephhensh2000@gmail.com>
2023-03-28 10:15:34 -0700

    Write function that draws a straight line in the terminal n number of times, using only _putchar func to print

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line: function that draws a straight line in the terminal
+ * based on n number of times
+ * @n: the number of times the character _ should be printed
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
