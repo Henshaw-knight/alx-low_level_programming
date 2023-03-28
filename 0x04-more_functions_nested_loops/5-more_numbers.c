29959dd71886cdd07f2cda54ec01f00df6ab4726
Henshaw <josephhensh2000@gmail.com>
2023-03-28 08:21:17 -0700

    Update file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,7 +14,17 @@ void more_numbers(void)
 	for (i = 0; i <= 14; i++)
 	{
 		for (j = 0; j <= 14; j++)
-			_putchar(j);
+		{
+			if (j <= 9)
+			{
+				_putchar(j);
+			}
+			else
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+		}
 		_putchar('\n');
 	}
 }
