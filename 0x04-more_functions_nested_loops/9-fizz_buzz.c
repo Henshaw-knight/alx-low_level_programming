2497adbb9fe6204c5c309270896ee4efb897dfb2
Henshaw <josephhensh2000@gmail.com>
2023-03-29 06:20:05 -0700

    Write program for FizzBuzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for program that prints the numbers from 1 to 100
+ * followed by a new line. Fizz is printed for multiples of 3, Buzz for
+ * multiples of 5 and FizzBuzz for multiples of 3 and 5.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 100)
+		{
+			printf("Buzz\n");
+		}
+		else
+		{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+		}
+	}
+}
