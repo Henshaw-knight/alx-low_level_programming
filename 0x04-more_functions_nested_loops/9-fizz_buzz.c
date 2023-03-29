258e398841b76884b1755f1d94b955f4c64a2f85
Henshaw <josephhensh2000@gmail.com>
2023-03-29 06:54:53 -0700

    Update code flow

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,9 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
 		{
@@ -21,8 +23,6 @@ int main(void)
 			else
 				printf("Buzz ");
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
 		else
 			printf("%d ", i);
 	}
