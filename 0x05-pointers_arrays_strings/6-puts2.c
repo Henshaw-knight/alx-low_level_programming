1fd7fa286f14f5f57fcb618f122c6f540e8de5b5
Henshaw <josephhensh2000@gmail.com>
2023-04-01 06:47:03 -0700

    Update file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,14 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	for ( ; str[i] != '\0'; )
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
-
-		i = i + 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
