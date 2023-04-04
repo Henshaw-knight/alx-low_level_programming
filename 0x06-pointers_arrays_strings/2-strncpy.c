cdb2e36fbdd1cfd5ec60adcb04eab7e846df11de
Henshaw <josephhensh2000@gmail.com>
2023-04-04 09:42:08 -0700

    Update file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,12 +13,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && *dest; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[0] = src[0];
-		dest++;
+		dest[i] = src[i];
 	}
-	*dest = '\0';
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
