6fdf036a4c45ed67dbe7bf9157805e0dcdc56c53
Henshaw <josephhensh2000@gmail.com>
2023-04-07 04:56:52 -0700

    Update file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,25 +9,19 @@
  */
 char *cap_string(char *str)
 {
-	int i = 0;
+	int i;
 
-	if (*str >= 'a' && *str <= 'z')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		*str = *str - 32;
-	}
-
-	for (i = 1; str[i] != '\0'; i++)
-	{
-		char c = *(str--);
-
-		if (c == ',' || c == ';' || c == '.' || c == '!'
-				|| c == '?' || c == '"' || c == '('
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			char c = *(str - 1);
+		if (str[i] == str[0] || c == ',' || c == ';' || c == '.'
+				|| c == '!' || c == '?' || c == '"' || c == '('
 				|| c == ')' || c == '{' || c == '}'
 				|| c == ' ' || c == '\t' || c == '\n'
 		   )
-		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] = str[i] - 32;
+			str[i] = str[i] - 32;
 		}
 	}
 
