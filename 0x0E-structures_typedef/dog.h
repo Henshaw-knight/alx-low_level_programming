e494d91952c16ea9df37356a5dd9a2e1cbcea003
Henshaw <josephhensh2000@gmail.com>
2023-06-21 19:38:36 -0700

    Create header file and add struct for dog with necessary elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_MACRO
+#define DOG_MACRO
+/**
+ * struct dog - a struct to define a dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ *
+ * Description: this structure gives information about a dog and who
+ * its owner is.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_MACRO */
