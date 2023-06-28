bf35d0d00fee0b9051a0a8cfbe63b324e3f32f57
Henshaw <josephhensh2000@gmail.com>
2023-06-28 16:33:52 -0700

    Update file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,6 +12,9 @@
  */
 int main(int argc, char *argv[])
 {
+	int num1;
+	int num2;
+
 	int (*op_func)(int, int);
 
 	if (argc < 4 || argc > 4)
@@ -19,21 +22,18 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	else
+
+	op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL)
 	{
-		int num1 = atoi(argv[1]);
-		int num2 = atoi(argv[3]);
-		char *operation = argv[2];
-
-		op_func = get_op_func(operation);
-
-		if (op_func == NULL)
-		{
-			printf("Error\n");
-			exit(99);
-		}
-
-		printf("%d\n", op_func(num1, num2));
+		printf("Error\n");
+		exit(99);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	printf("%d\n", op_func(num1, num2));
 	return (0);
 }
