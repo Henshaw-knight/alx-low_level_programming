00d175b814d2d9e29c803633fa43da3d9d36d9c9
Henshaw <josephhensh2000@gmail.com>
2023-06-28 16:41:14 -0700

    Update file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,6 +23,12 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	op_func = get_op_func(argv[2]);
 
 	if (op_func == NULL)
