fc3249bfb32833d711d3a1681c310d9d3809fcf3
Henshaw <josephhensh2000@gmail.com>
2023-06-29 00:50:50 -0700

    Write function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs
+ *
+ * @d: pointer the dog structure
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+
+	free(d);
+}
