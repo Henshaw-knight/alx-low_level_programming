716ab60a530fcb232e92610da3454e410aab1d93
Henshaw <josephhensh2000@gmail.com>
2023-07-05 14:00:35 -0700

    Write function that compares two strings and returns 1 if the strings can be considered identical, otherwise 0. s2 string can contain the special character * and the * can replace any string including empty string

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * wildcmp - function that compares two strings and checks if they are
+ * identical or not
+ *
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: 1 if strings are identical, otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2)
+		return (wildcmp(s1++, s2++));
+
+	if (!*s1 && !*s2)
+		return (1);
+
+	if (*s2 == '*' && *(s1 + 1) && *s2)
+		return (0);
+
+	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+		return (1);
+
+	return (0);
+}
