7f002e9182880f5484ce513cf3eebd13a82c4e60
Henshaw <josephhensh2000@gmail.com>
2023-07-05 14:03:27 -0700

    Update file

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,11 +11,11 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == *s2)
-		return (wildcmp(s1++, s2++));
-
 	if (!*s1 && !*s2)
 		return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1++, s2++));
 
 	if (*s2 == '*' && *(s1 + 1) && *s2)
 		return (0);
