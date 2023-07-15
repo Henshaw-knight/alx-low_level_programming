a14075a94249f89efd0e26581daf27d9a2ff0d5d
Henshaw <josephhensh2000@gmail.com>
2023-07-14 19:45:55 -0700

    Write function that allocates memory for an array, using malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - function that allocates memory for an array using malloc
+ *
+ * @nmemb: the number of elements of the array
+ * @size: the size in bytes for each element
+ *
+ * Return: pointer to allocated memory on success, NULL otherwise
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		arr[i] = 0;
+
+	return (arr);
+}
