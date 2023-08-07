0073da72c5963a230bc848c929952af0d84ef06d
Henshaw <josephhensh2000@gmail.com>
2023-08-07 06:08:39 -0700

    Update file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -13,7 +13,7 @@ void print_binary(unsigned long int n)
 	unsigned int i = 0;
 	int flag = 0;
 	unsigned long int largest = 1UL << ((sizeof(unsigned long int) * 8) - 1);
-	while (i < sizeof(unsigned long int * 8))
+	while (i < sizeof(unsigned long int) * 8)
 	{
 		if ((largest & n) != 0)
 			flag = 1;
