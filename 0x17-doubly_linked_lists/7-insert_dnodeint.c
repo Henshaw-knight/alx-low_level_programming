fa06626c9519a13b88ec51e58c3a48458d8ff386
Henshaw <josephhensh2000@gmail.com>
2023-10-27 23:37:09 -0700

    Update file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -43,7 +43,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 		return (new_node);
 	}
-	while (position < idx)
+	while (position != idx)
 	{
 		temp = temp->next;
 		position++;
